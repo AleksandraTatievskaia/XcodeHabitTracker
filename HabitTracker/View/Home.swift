894f3a5928ecd77cdaa5428a8b707bae4e3634a3
AleksandraTatievskaia <alexschool04@mail.ru>
2025-03-29 21:01:59 +0500

    Home screen add btn + other buttons

---
+++ HabitTracker/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  HabitTracker
+//
+//  Created by Александра Татиевская on 29.03.2025.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
