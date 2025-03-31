3c06cac1e0cd2229b3c55c59fd736bbf4506580a
AleksandraTatievskaia <alexschool04@mail.ru>
2025-03-31 20:19:58 +0500

    AddNewHabit готовый и рабочий View

---
+++ HabitTracker/View/AddNewHabit.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewHabit.swift
+//  HabitTracker
+//
+//  Created by Александра Татиевская on 31.03.2025.
+//
+
+import SwiftUI
+
+struct AddNewHabit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewHabit()
+}
---
+++ HabitTracker/ViewModel/HabitViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  HabitViewModel.swift
+//  HabitTracker
+//
+//  Created by Александра Татиевская on 31.03.2025.
+//
+
+import SwiftUI
+
+struct HabitViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitViewModel()
+}
